63d1b034896f0825e41b20566a083954e181cd55
Angel3101 <angeljessbr31@gmail.com>
2021-03-05 21:32:09 -0600

    Practica 3 Zapateria

---
+++ Practica3Java/src/Zapatos.java
@@ -0,0 +1,107 @@
+
+/*******************************************************************************
+ * 2021, All rights reserved.
+ *******************************************************************************/
+
+import java.util.HashSet;
+// Start of user code (user defined imports)
+
+// End of user code
+
+/**
+ * Description of Zapatos.
+ * 
+ * @author Admin
+ */
+public class Zapatos {
+	/**
+	 * Description of the property Clave.
+	 */
+	private HashSet<int>Clave=new HashSet<int>();
+
+	/**
+	 * Description of the property Material.
+	 */
+	private HashSet<String> Material = new HashSet<String>();
+
+	/**
+	 * Description of the property Troquel.
+	 */
+	private HashSet<String> Troquel = new HashSet<String>();
+
+	/**
+	 * Description of the property CantProdxDia.
+	 */
+	private HashSet<int>CantProdxDia=new HashSet<int>();
+
+	/**
+	 * Description of the property Colores.
+	 */
+	private HashSet<String> Colores = new HashSet<String>();
+
+	// Start of user code (user defined attributes for Zapatos)
+
+	// End of user code
+
+	/**
+	 * The constructor.
+	 */
+	public Zapatos() {
+		// Start of user code constructor for Zapatos)
+		super();
+		// End of user code
+	}
+
+	/**
+	 * Description of the method CostoxLote: float  (CostoXunidad).
+	 */
+	public void CostoxLote:float(CostoXunidad)()
+	{
+		// Start of user code for method CostoxLote: float  (CostoXunidad)
+		// End of user code
+	}
+
+	// Start of user code (user defined methods for Zapatos)
+
+	// End of user code
+	/**
+	 * Returns Clave.
+	 * @return Clave 
+	 */
+	public HashSet<int> getClave() {
+		return this.Clave;
+	}
+
+	/**
+	 * Returns Material.
+	 * @return Material 
+	 */
+	public HashSet<String> getMaterial() {
+		return this.Material;
+	}
+
+	/**
+	 * Returns Troquel.
+	 * @return Troquel 
+	 */
+	public HashSet<String> getTroquel() {
+		return this.Troquel;
+	}
+
+	/**
+	 * Returns CantProdxDia.
+	 * @return CantProdxDia 
+	 */
+	public HashSet<int> getCantProdxDia() {
+		return this.CantProdxDia;
+	}
+
+	/**
+	 * Returns Colores.
+	 * @return Colores 
+	 */
+	public HashSet<String> getColores() {
+		return this.Colores;
+	}
+
+}
